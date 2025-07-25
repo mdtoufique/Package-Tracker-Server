f904525d52a6eb3366410f7d469a565cf9f9acc4
toufique <mdrehmant@gmail.com>
2025-07-26 04:39:28 +0600

    debug

--- src/utils/db.js
+++ src/utils/db.js
@@ -5,7 +5,7 @@ import dotenv from 'dotenv';
 const connectDB = async () => {
   try {
     const conn = await mongoose.connect(process.env.MONGO_URI);
-    console.log(`✅ MongoDB connected: ${conn.connection.host}`);
+    console.log(`✅ MongoDB connected: ${conn.connection.name}`);
   } catch (error) {
     console.error('❌ MongoDB connection failed:', error.message);
     process.exit(1);
