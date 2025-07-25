223afff2d695a081497ebe5712d8833caedd0782
toufique <mdrehmant@gmail.com>
2025-07-26 04:14:56 +0600

    debug

--- api/index.js
+++ api/index.js
@@ -1,19 +1,8 @@
-// api/index.js
-import fs from 'fs';
-import path from 'path';
-import { fileURLToPath } from 'url';
+
 import app from '../src/app.js';
 import connectDB from '../src/utils/db.js';
 import { startAlertService } from '../src/services/alertService.js';
 
-// Debug: Log file structure
-console.log('Files in /var/task:', fs.readdirSync('/var/task'));
-try {
-  console.log('Files in /var/task/src:', fs.readdirSync('/var/task/src'));
-  console.log('Resolved path for app.js:', path.resolve(fileURLToPath(import.meta.url), '../src/app.js'));
-} catch (err) {
-  console.log('Error reading /var/task/src:', err.message);
-}
 
 let isInitialized = false;
 
