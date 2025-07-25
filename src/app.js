24588b67f72add0579fa1a26f152fecd0c51d44f
toufique <mdrehmant@gmail.com>
2025-07-26 05:30:01 +0600

    bug fix

--- src/app.js
+++ src/app.js
@@ -7,7 +7,7 @@ import { verifyApiToken } from "./middlewares/auth.js";
 const app = express();
 
 app.use(cors({
-  origin: 'http://localhost:5173', 
+  origin: ['http://localhost:5173','http://package-tracker-client.vercel.app'], 
   methods: ['GET', 'POST'],
 }));
 app.use(express.json());
